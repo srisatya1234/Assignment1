71ee87c35bc329a96535e48f72b5e4767f795bcc
Tatipamula Sri Satya <srisatya1197@email.com>
2023-03-04 08:31:10 +0530

    New Files

---
+++ src/config.py
@@ -0,0 +1,10 @@
+CONFIG = {
+    "database": {
+        "gnits": {
+            "user": "aiworkshop",
+            "password": "Gnits@123",
+            "host": "185.217.126.61",
+            "database": "studentworks"
+        }
+    }
+}
